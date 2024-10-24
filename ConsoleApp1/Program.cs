bcd13374cad4d18f2eaed05ab777b7db08fb388a
Damian Kontek <dkontek@wsiz.edu.pl>
2024-10-24 15:03:44 +0200

    .

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,11 +4,57 @@
     {
         static void Main(string[] args)
         {
+            /*            Console.WriteLine("Podaj liczbę");
+                        var input = Console.ReadLine();
+
+                        Console.WriteLine(IsEven(int.Parse(input)) ? "Parzysta" : "Nieparzysta");
+                       */
+
+            while (true)
+            {
+                Console.WriteLine("Wybierz opcję");
+                Console.WriteLine("1. Czy Parzysta");
+                Console.WriteLine("2. Wypisz Parzyste");
+                Console.WriteLine("3. Silnia");
+                Console.WriteLine("0. Wyjście");
+
+                var input = int.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        {
+                            CheckEven();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("Podaj liczbe");
+                            var x = int.Parse(Console.ReadLine());
+                            Console.WriteLine(Silnia(x));
+                            break;
+                        }
+                }
+
+                if (input == 0) break;
+            }
+        }
+
+
+        static int Silnia(int x)
+        {
+            if (x == 0) return 1;
+
+            return Silnia(x - 1) * x;
+        }
+
+        static void CheckEven()
+        {
             Console.WriteLine("Podaj liczbę");
             var input = Console.ReadLine();
 
             Console.WriteLine(IsEven(int.Parse(input)) ? "Parzysta" : "Nieparzysta");
-           
         }
 
 
