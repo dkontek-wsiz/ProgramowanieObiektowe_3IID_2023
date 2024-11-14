7554a3087fa288817a7eb1de83e072f362763d49
Damian Kontek <dkontek@wsiz.edu.pl>
2024-11-14 14:13:47 +0100

    .

---
+++ Lab2/Models/Person.cs
@@ -0,0 +1,74 @@
+﻿namespace Lab2.Models
+{
+    internal class Person
+    {
+
+        private string name;
+        private string surname;
+        private int age;
+        private string pesel;
+
+        public string Name { get { return name; } 
+            set {
+                Console.Write(value);
+                name = value; 
+            } }
+
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public int Age { get { return age; } set
+            {
+                if(value < 0) age = 0;
+                else age = value;
+            }
+        }
+
+        public string Pesel => pesel;//{ get { return pesel; } }
+
+
+        public Person()
+        {
+            name = "Test";
+        }
+
+
+        public Person(string name2)
+        {
+            name = name2;
+        }
+
+        public Person(int name2)
+        {
+            name = name2.ToString();
+        }
+
+        public void Welcome()
+        {
+            Console.WriteLine($"Witaj. Jestem {name}");
+        }
+
+        public void Welcome(string name2)
+        {
+            Console.WriteLine("Witaj");
+        }
+
+        public void Welcome(int x)
+        {
+            Console.WriteLine("Witaj");
+        }
+
+        public string Welcome(string name2, string name3)
+        {
+            Console.WriteLine("Witaj");
+            return "Test";
+        }
+
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab2.Models;
+
+Console.WriteLine("Hello, World!");
+
+Person o = new Person("Testy");
+
+o.Welcome();
